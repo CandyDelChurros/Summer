2b5171260fe14241c1f0274e88ce0fd42efb1549
Cayo <Cayo.veiga54@gmail.com>
2025-06-05 21:43:37 -0400

    esqueleto index pronto

--- summer/src/app/page.tsx
+++ summer/src/app/page.tsx
@@ -1,6 +1,124 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <header></header>
+    <div>
+      <Header></Header>
+      <Navbar></Navbar>
+      <main>
+        <div></div>
+        <div>
+          <div><span>Explore Lugares Novos!</span></div>
+          <div>
+            <div>
+              <div></div>
+              <span>Rio de Janeiro</span>
+              <span>Ofertas de até 30% off</span>
+            </div>
+            <div>
+              <div></div>
+              <span>Salvador</span>
+              <span>Ofertas de até 20% off</span>
+            </div>
+            <div>
+              <div></div>
+              <span>São Paulo</span>
+              <span>Ofertas de até 80% off</span>
+            </div>
+            <div>
+              <div></div>
+              <span>Angra dos Reis</span>
+              <span>Ofertas de até 60% off</span>
+            </div>
+            <div>
+              <div></div>
+              <span>Porto de Galinhas</span>
+              <span>Ofertas de até 50% off</span>
+            </div>
+          </div>
+          <div>
+            <div><span>Pacotes para uma Viagem Longa e Divertida</span></div>
+            <div>
+              <div>
+                <div>
+                  <span>Maragogi - AL</span>
+                </div>
+                <div>
+                  <div>
+                    <span>Saindo de</span>
+                    <span>Preços a partir de:</span>
+                  </div>
+                  <div>
+                    <span>São Paulo</span>
+                    <span>R$ 821,00</span>
+                  </div>
+                  <div>
+                    <span>Rio de Janeiro</span>
+                    <span>R$ 650,00</span>
+                  </div>
+                  <div>
+                    <span>Campo Grande</span>
+                    <span>R$ 925,00</span>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <span>Gramado - RS</span>
+                </div>
+                <div>
+                  <div>
+                    <span>Saindo de</span>
+                    <span>Preços a partir de:</span>
+                  </div>
+                  <div>
+                    <span>São Paulo</span>
+                    <span>R$ 821,00</span>
+                  </div>
+                  <div>
+                    <span>Rio de Janeiro</span>
+                    <span>R$ 650,00</span>
+                  </div>
+                  <div>
+                    <span>Campo Grande</span>
+                    <span>R$ 925,00</span>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <span>Xique Xique - BA</span>
+                </div>
+                <div>
+                  <div>
+                    <span>Saindo de</span>
+                    <span>Preços a partir de:</span>
+                  </div>
+                  <div>
+                    <span>São Paulo</span>
+                    <span>R$ 821,00</span>
+                  </div>
+                  <div>
+                    <span>Rio de Janeiro</span>
+                    <span>R$ 650,00</span>
+                  </div>
+                  <div>
+                    <span>Campo Grande</span>
+                    <span>R$ 925,00</span>
+                  </div>
+                </div>
+                <div>
+                  <div></div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer></Footer>
+    </div>
   );
 }
